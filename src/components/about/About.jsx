0057033295e180b30593809e678d657aa24bd496
Antonela <antonelabelenrodriguez@gmail.com>
2022-12-03 18:10:25 -0300

    Experience section completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             <div className="container about__container">
                 <div className="about__me">
                     <div className="about__me-image">
-                        <img src={myImage} alt="about image" />
+                        <img src={myImage} alt="about description" />
                     </div>
                 </div>
                 
