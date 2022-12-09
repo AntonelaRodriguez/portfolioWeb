9a8bdf44aeab5ebf64fd14254f77a95c8d814c46
Antonela <antonelabelenrodriguez@gmail.com>
2022-12-09 18:22:54 -0300

    Skills and Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,63 @@
 import React from "react";
 import './portfolio.css';
+import IMG1 from '../../assets/509be709-8b7f-4ce1-8089-babf11141785.png'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: "This is a porfolio item title",
+        github: 'https://github.com/AntonelaRodriguez',
+        demo: 'https://integrador-dise-o-6qskoydhx-antonelarodriguez.vercel.app/'
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: "This is a porfolio item title",
+        github: 'https://github.com/AntonelaRodriguez',
+        demo: 'https://integrador-dise-o-6qskoydhx-antonelarodriguez.vercel.app/'
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: "This is a porfolio item title",
+        github: 'https://github.com/AntonelaRodriguez',
+        demo: 'https://integrador-dise-o-6qskoydhx-antonelarodriguez.vercel.app/'
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: "This is a porfolio item title",
+        github: 'https://github.com/AntonelaRodriguez',
+        demo: 'https://integrador-dise-o-6qskoydhx-antonelarodriguez.vercel.app/'
+    },
+];
 
 const Portfolio = () => {
     return(
-        <section id="porfolio">Portfolio</section>
+        <section id="porfolio">
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+               {
+                data.map(({id, image, title, github, demo}) => {
+                    return(
+                    <article key={id} className="portfolio__item">
+                        <div className="portfolio__item-image">
+                            <img src={image} alt={title} />
+                        </div>
+                        <h3>{title}</h3>
+                        <div className="portfolio__item-cta">
+                            <a href={github} className="btn" target='_blank'>Github</a>
+                            <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+                        </div>
+                    </article>
+                    )
+                })
+               }
+            </div>
+        </section>
     );
 };
 
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,90 @@
 import React from "react";
 import './skills.css';
+import {BsCheck2Square} from 'react-icons/bs'
 
 const Skills = () => {
     return(
-        <section id="skills">Skills</section>
+        <section id="skills">
+            <h5>What are my skills</h5>
+            <h2>Skills</h2>
+
+            <div className="container skills__container">
+                <article className="skills">
+                    <div className="skills__head">
+                        <h3>UX/UI design</h3>
+                    </div>
+
+                    <ul className="skills__list">
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                    </ul>
+                </article>
+
+                <article className="skills">
+                    <div className="skills__head">
+                        <h3>Content creation</h3>
+                    </div>
+
+                    <ul className="skills__list">
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                    </ul>
+                </article>
+
+                <article className="skills">
+                    <div className="skills__head">
+                        <h3>Web Develoment</h3>
+                    </div>
+
+                    <ul className="skills__list">
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <BsCheck2Square className="skills__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
     );
 };
 
